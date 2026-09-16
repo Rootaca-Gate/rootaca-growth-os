90ff7f41a46f96f90cd332a64504effc57004ba3
smartdevco <dev@smartdevco.com>
2026-09-16 16:08:05 +0300

    add cores again

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,21 @@
+import { Logger } from 'nestjs-pino';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
-import { createApp } from './create-app';
+import { AppModule } from './app.module';
+import { configureApp } from './configure-app';
 import { EnvironmentVariables } from './common/config/env.validation';
 
 async function bootstrap(): Promise<void> {
-  const app = await createApp();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(Logger));
+  app.set('trust proxy', 1);
+  configureApp(app);
   const config = app.get(ConfigService<EnvironmentVariables, true>);
   const port = config.get('PORT', { infer: true });
-  await app.listen(port);
+  await app.listen(process.env.PORT ?? port);
 }
 
 void bootstrap();
